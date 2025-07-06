3798294fd9f8dd37f3246486b0ff3ae817a79766
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-07-06 19:28:33 +0600

    fix: prisma instance

---
+++ app/actions/create-order.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { prisma } from '@/lib/prisma';
+
+
+export async function createOrder(data: any) {
+
+
+  // const res = await prisma.order.create({
+  //   data: data
+  // })
+  
+
+}
