e13a1368c255cb3f75a4318dc279d356fa1bb86e
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-07-07 20:01:39 +0600

    feat: added login and register page

--- schema/index.ts
+++ schema/index.ts
@@ -1,29 +1,46 @@
-import z from 'zod';
+import z from "zod";
 
 export const orderFormSchema = z.object({
   fullName: z.string().min(2, {
     message: "Enter a valid name.",
   }),
-
   phone: z
     .string()
     .min(11, { message: "Phone is required." })
     .regex(/^(\+8801|8801|01)[0-9]{9}$/, {
       message: "Invalid Bangladeshi number.",
     }),
-
   address: z.string().min(5, {
     message: "Enter a valid address.",
   }),
-
   city: z.string().min(2, {
     message: "Enter a valid city.",
   }),
-
   zipCode: z
     .string()
     .length(4, { message: "Zip must be 4 digits." })
     .regex(/^[0-9]{4}$/, {
       message: "Zip must be numeric.",
     }),
+});
+
+export const loginSchema = z.object({
+  email: z.string().min(2, {
+    message: "Enter a valid name.",
+  }),
+  password: z.string().min(2, {
+    message: "Enter a valid name.",
+  }),
+});
+
+export const registerSchema = z.object({
+  username: z.string().min(2, {
+    message: "Enter a valid name.",
+  }),
+  email: z.string().min(2, {
+    message: "Enter a valid name.",
+  }),
+  password: z.string().min(2, {
+    message: "Enter a valid name.",
+  }),
 });
