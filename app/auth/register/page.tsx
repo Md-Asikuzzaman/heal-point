7510879c5746d3e65bbbc3378187be5e1ad1e3dd
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-07-20 20:09:57 +0600

    fix: suspense issues

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from "react";
 import RegisterForm from "../_components/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <>
+    <Suspense
+      fallback={
+        <div className="text-center py-2">Register Form Loading...</div>
+      }
+    >
       <RegisterForm />
-    </>
+    </Suspense>
   );
 }
